191ffcaefa859fd6108edb7da8e594d7af73e60f
yogeshwaray <yashaswi.yogeshwara@campsystems.com>
2021-12-02 09:37:34 +0530

    Basic Web app using javascipt signalR client

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessageAsync(string user, string message) {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
